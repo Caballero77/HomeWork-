b997f0db03c10caad2aa7604758fdced650a9971
Caballero77 <caballiero777@gmail.com>
2016-08-19 21:13:05 +0300

    Add new tests and fix bug in old test

--- Classes/Matrix.cs
+++ Classes/Matrix.cs
@@ -44,3 +44,84 @@ namespace HomeWork.Classes
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Розв'язки =)
+ * return matrix.Sum((m) => m.Sum());
+ * return (double)(this.Sum(matrix)) / matrix.Sum((m) => m.Length);
+ * int r = 0;
+            int t = 0;
+            foreach (var vec in matrix)
+            {
+                r++;
+                foreach (var elem in vec) t++;
+            }
+            t /= r;
+
+            int[][] result = new int[t][];
+
+            for (var i = 0; i < t; i++)
+            {
+                result[i] = new int[r];
+            }
+
+            for (var i = 0; i < r; i++)
+            {
+                for (var j = 0; j < t; j++)
+                {
+                    result[j][i] = matrix[i][j];
+                }
+            }
+
+            return result;
+     */
--- HomeWork.Tests/MatrixTests.cs
+++ HomeWork.Tests/MatrixTests.cs
@@ -2,11 +2,27 @@
 {
     using System;
     using NUnit.Framework;
+    using System.Collections.Generic;
     using HomeWork.Classes;
 
     [TestFixture]
     public class MatrixTests
     {
+        #region Matrixes
+
+        private List<int[][]> matrixes = new List<int[][]>
+        {
+            new int[][]{ new[] { 1, 2, 3 },
+                         new[] { 4, 5, 6 },
+                         new[] { 7, 8, 9} },
+
+            new int[][]{ new[] { 1, 2, 3 },
+                         new[] { 3, 2, 1 },
+                         new[] { 3, 1, 2 } }
+        };
+
+        #endregion
+
         #region Sum Tests
         private Matrix matrix = new Matrix();
 
@@ -76,7 +92,7 @@
 
             var result = this.matrix.ArithmeticMean(matrix);
 
-            Assert.AreEqual(expecterResult, result);
+            Assert.True(Math.Abs(result - expecterResult) <= 0.00001);
         }
 
         [Test]
@@ -87,18 +103,18 @@
 
             var result = this.matrix.ArithmeticMean(matrix);
 
-            Assert.AreEqual(expecterResult, result);
+            Assert.True(Math.Abs(result - expecterResult) <= 0.00001);
         }
 
         [Test]
         public void ArithmeticMean_3()
         {
             var matrix = new int[][] { new[] { 0, 0, 0, 0, 0 }, new[] { 2, 2, 2, 2, 2 } };
-            var expecterResult = 6d/12;
+            var expecterResult = 10d/10;
 
             var result = this.matrix.ArithmeticMean(matrix);
 
-            Assert.AreEqual(expecterResult, result);
+            Assert.True(Math.Abs(result - expecterResult) <= 0.00001);
         }
 
         [Test]
@@ -109,7 +125,64 @@
 
             var result = this.matrix.ArithmeticMean(matrix);
 
-            Assert.AreEqual(expecterResult, result);
+            Assert.True(Math.Abs(result - expecterResult) <= 0.00001);
+        }
+
+        #endregion
+
+        #region Transposition Tests
+
+        [Test]
+        public void Transposition_1()
+        {
+            var matrix = new int[][] { new[] { 1, 2, 3 },
+                                       new[] { 3, 2, 1 },
+                                       new[] { 3, 1, 2 } };
+
+            var matrixResult = new int[][] { new[] { 1, 3, 3 },
+                                             new[] { 2, 2, 1 },
+                                             new[] { 3, 1, 2 } };
+
+            var result = this.matrix.Transposition(matrix);
+
+            bool res = true; 
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    res &= result[i][j] == matrixResult[i][j];
+                }
+            }
+            Assert.True(res);
+
+        }
+
+        [Test]
+        public void Transposition_2()
+        {
+            var matrix = new int[][] { new[] { 1, 2, 3 },
+                                       new[] { 3, 2, 1 },
+                                       new[] { 3, 1, 2 },
+                                       new[] { 2, 1, 3 }};
+
+            var matrixResult = new int[][] { new[] { 1, 3, 3, 2 },
+                                             new[] { 2, 2, 1, 1 },
+                                             new[] { 3, 1, 2, 3 } };
+
+            var result = this.matrix.Transposition(matrix);
+
+            bool res = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    res &= result[i][j] == matrixResult[i][j];
+                }
+            }
+            Assert.True(res);
+
         }
 
         #endregion
